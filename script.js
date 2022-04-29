df0a31b343ecd945284de7d9ee393f6b1a10ec6e
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-29 18:00:52 +0200

    most finish

--- script.js
+++ script.js
@@ -28,6 +28,9 @@ console.log(data)
 const selectTag = document.querySelector('select');
 console.log(selectTag);
 
+const bana = document.querySelector('.bana');
+
+
 const animals = data.filter(obj=>(obj.type === "animal")? true: false);
 console.log(animals ,"animals")
 
@@ -39,29 +42,28 @@ console.log(vegetable ,"vegetable")
 const user = data.filter(obj=> (obj.type === "user")?true : false);
 console.log(user ,"user")
 
-selectTag.addEventListener('change', function(){
+selectTag.addEventListener('change', ()=>{
     console.log(selectTag.value);
-    console.log(data)
-    
+(selectTag.value === "all")? resetPrint(data) :(selectTag.value === "animals")?  resetPrint(animals) :(selectTag.value === "vegetable")?  resetPrint(vegetable)   : resetPrint(user); 
 })
 
 
 function prntHtml(array){
-    const row = document.querySelector('.row')
-    console.log(row)
+    console.log(bana)
     array.forEach(obj => {
-        const {color, name, prefix,} = obj
-        row.innerHTML += `
-        <!-- CARD  -->
-        <div class="col-12 col-md-6 col-lg-2">
-            <div class="card text-center" style="color: ${color}">
-                <h1><i class="${prefix}-solid ${prefix}-${name} "></i></h1>
+        const {color, name, prefix,} = obj;
+        bana.innerHTML += `
+        <div class="col-12 col-md-6 col-lg-2 ">
+            <div class="card text-center pb-5 pt-5" style="color: ${color}">
+                <h1><i class="${prefix}-solid ${prefix}${name} "></i></h1>
                 <p>${name.charAt(0).toUpperCase() + name.slice(1)}</p>
             </div>
         </div>
-        <!--END CARD  -->
         `;
     });
 }
 
-prntHtml(data)
+function resetPrint(array){
+    bana.innerHTML = ""
+    prntHtml(array)
+}
