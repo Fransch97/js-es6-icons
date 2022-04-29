952e98bfdbf44eed0d81dd3da2c886b80b463153
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-29 19:29:20 +0200

    .

--- script.js
+++ script.js
@@ -1,21 +1,18 @@
-console.log(data)
 const aF = ["A","B","C","D","E","F"];
-
+//get select
 const selectTag = document.querySelector('select');
-
+//get row
 const bana = document.querySelector('.bana');
-
+//creat arrays
 const animals = data.filter(obj=>(obj.type === "animal")? true: false);
-
 const vegetable = data.filter(obj=>(obj.type === "vegetable")? true:false);
-
 const user = data.filter(obj=> (obj.type === "user")?true : false);
-
+//color number # length
 const numberLength = 6;
-resetPrint(data);
-
+//event change
 selectTag.addEventListener('change', ()=>setInterval(()=>(selectTag.value === "all")? resetPrint(data) :(selectTag.value === "animals")?  resetPrint(animals) :(selectTag.value === "vegetable")?  resetPrint(vegetable)   : resetPrint(user),100));
-
+//reset at start and under the funciton's
+resetPrint(data);
 function prntHtml(array){array.forEach(obj => { const {color, name, prefix} = obj;
         bana.innerHTML += `
         <div class="col-12 col-md-4 col-lg-2 mt-4 mb-4">
@@ -29,9 +26,8 @@ function prntHtml(array){array.forEach(obj => { const {color, name, prefix} = ob
 };
 
 function resetPrint(array){ bana.innerHTML = ""; prntHtml(array)};
-//generate random number
+//generate random color all functions
 function rndNumber(max){ return Math.floor(Math.random()*max)};
-//generate ramìndom abc
 function randmAf(array){ return array[rndNumber(array.length)] };
 function numbrOrAf(array, max){ return (rndNumber(2) > 0)? rndNumber(max): randmAf(array)};
 function randomColor(array,max){ const nA = []; for(i = 0; i< numberLength; i++){nA.push(numbrOrAf(aF, 10))};const color = nA.join(""); return color}
