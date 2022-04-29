b7bf7b6db54866c5dbbf7f1a9f2e950155d81559
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-29 19:08:16 +0200

    di più e meglio al momento non mi viene in mente

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
-console.log("yes master");
-
-// **Milestone 1**
-// Partendo dalla struttura dati fornita, visualizzare in pagina un box per ogni icona, in cui è presente il nome dell’icona e l’icona stessa.
-// **Milestone 2**
-// Ciascuna icona ha una proprietà “color”: utilizzare questa proprietà per visualizzare le icone del colore corrispondente.
-// **Milestone 3**
-// Aggiungere alla pagina una select in cui le options corrispondono ai vari tipi di icone *(animal, vegetable, user)*. Quando l’utente seleziona un tipo dalla select, visualizzare solamente le icone corrispondenti.
-// **BONUS**
-// 1- modificare la struttura dati fornita e valorizzare la proprietà “color” in modo dinamico: generare in modo casuale un codice colore, sapendo che la notazione esadecimale è formata dal simbolo “#” seguito da 6 caratteri alfanumerici compresi tra 0 e 9 e A e F.
-// 2- popolare le options della select della milestone 3 dinamicamente.
-
 console.log(data)
+const aF = ["A","B","C","D","E","F"];
 
 const selectTag = document.querySelector('select');
 console.log(selectTag);
@@ -25,29 +14,36 @@ console.log(vegetable ,"vegetable")
 
 const user = data.filter(obj=> (obj.type === "user")?true : false);
 console.log(user ,"user")
-resetPrint(data)
-selectTag.addEventListener('change', ()=>{
-    console.log(selectTag.value);
-(selectTag.value === "all")? resetPrint(data) :(selectTag.value === "animals")?  resetPrint(animals) :(selectTag.value === "vegetable")?  resetPrint(vegetable)   : resetPrint(user); 
-})
 
-function prntHtml(array){
-    console.log(bana)
-    array.forEach(obj => {
-        const {color, name, prefix, family} = obj;
+const numberLength = 6
+resetPrint(data)
+
+selectTag.addEventListener('change', ()=>setInterval(()=>(selectTag.value === "all")? resetPrint(data) :(selectTag.value === "animals")?  resetPrint(animals) :(selectTag.value === "vegetable")?  resetPrint(vegetable)   : resetPrint(user) 
+,100))
+
+function prntHtml(array){array.forEach(obj => { const {color, name, prefix} = obj;
         bana.innerHTML += `
-        <div class="col-12 col-md-6 col-lg-2 mt-4 mb-4">
-            <div class="card text-center pb-5 pt-5" style="color: ${color}">
+        <div class="col-12 col-md-4 col-lg-2 mt-4 mb-4">
+            <div class="card text-center pb-5 pt-5" style="color: #${randomColor()}">
                 <h1><i class="${prefix}solid ${prefix}${name} "></i></h1>
                 <p>${name.charAt(0).toUpperCase() + name.slice(1)}</p>
             </div>
         </div>
         `;
     });
-}
+};
 
-function resetPrint(array){
-    bana.innerHTML = ""
-    prntHtml(array)
+function resetPrint(array){ bana.innerHTML = ""; prntHtml(array)};
+//generate random number
+function rndNumber(max){ return Math.floor(Math.random()*max)};
+//generate ramìndom abc
+function randmAf(array){ return array[rndNumber(array.length)] };
+function numbrOrAf(array, max){ return (rndNumber(2) > 0)? rndNumber(max): randmAf(array)};
+function randomColor(array,max){ const nA = [];
+    for(i = 0; i< numberLength; i++){
+        nA.push(numbrOrAf(aF, 10))
+    }
+    const color = nA.join("")
+    console.log()
+    return color
 }
-
