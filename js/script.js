725e8c503c668c7b741c8552e81779e1fc32b234
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-30 22:18:36 +0200

    debug of all

--- js/script.js
+++ js/script.js
@@ -6,19 +6,17 @@ const types = ["all"], controlTypes = (arrToPush) =>{data.forEach(el => (!arrToP
 //insert dinamic into select the options                                                                                                     
 const options = (array)=>{array.forEach(el =>{const opt = document.createElement('option'); opt.value= el; opt.innerHTML = el; selectTag.append(opt)})}; options(types);
 // creare un arrary di arry di oggetti in base al array delle categorie
-const typesObjArr =[];
+const typesObjArr =[]; 
 // creare una funzione di un array basato sul array sopra scritto con un ciclo con un ciclo anidato
 const arrArrObj = (basearr,arrTopushArr, objlist)=>{basearr.forEach(types => arrTopushArr.push(objlist.filter(obj=>(obj.type === types))))}; arrArrObj(types,typesObjArr,data,data); console.log(typesObjArr)
 // creare una funzione che in base al tipo di valore selezionato stampa l'array degli oggetti giusto che stanno dentro al array di array di oggetti
-const conditionsChecker = (arrary,value)=> {arrary.forEach((array,index) =>{console.log(array.forEach(element=>{(element.type === value)? resetPrint(array): ""}))})}; conditionsChecker(typesObjArr,selectTag.value);
+const conditionsChecker = (arrary,value)=> {arrary.forEach(array =>{console.log(array.forEach(element=>{(element.type === value)? resetPrint(array): ""}))})}; conditionsChecker(typesObjArr,selectTag.value);
 //get row
 const row = document.querySelector('.bana');
 //color number # length
 const numberLength = 6;
-
 //event "change"
-selectTag.addEventListener('change',()=>setInterval(()=> {conditionsChecker(typesObjArr,selectTag.value)},100));//resetPrint(data)
-
+selectTag.addEventListener('change',()=>setInterval(()=> {(selectTag.value === "all")? resetPrint(data):conditionsChecker(typesObjArr,selectTag.value)},100));//resetPrint(data)
 //reset at start + under the funciton's
 resetPrint(data);
 function resetPrint(array){ row.innerHTML = ""; prntHtml(array)};
@@ -38,4 +36,3 @@ function rndNumber(max){ return Math.floor(Math.random()*max)};
 function randmAf(array){ return array[rndNumber(array.length)] };
 function numbrOrAf(array, max){ return (rndNumber(2) > 0)? rndNumber(max): randmAf(array)};
 function randomColor(array,max){ const nA = []; for(i = 0; i< numberLength; i++){nA.push(numbrOrAf(array, max))};const color = nA.join(""); return color};
-
