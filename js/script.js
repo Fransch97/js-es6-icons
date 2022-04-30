5a25edc477df479a388762ec580f60efdf245aec
Fransch97 <98567653+Fransch97@users.noreply.github.com>
2022-04-30 17:37:39 +0200

    changed row name xD

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ const selectTag = document.querySelector('select');
 //insert dinamic into select the options                                                                                                     
 const options = ()=>{const X = ['all','animals','vegetable','user'];  X.forEach(el =>{const opt = document.createElement('option'); opt.value= el; opt.innerHTML = el; selectTag.append(opt)})}; options();
 //get row
-const bana = document.querySelector('.bana');
+const row = document.querySelector('.bana');
 //creat arrays animals && vegetable && user
 const animals = data.filter(obj=>(obj.type === "animal")), vegetable = data.filter(obj=>(obj.type === "vegetable")),user = data.filter(obj=> (obj.type === "user"));
 //color number # length
@@ -13,9 +13,9 @@ const numberLength = 6;
 selectTag.addEventListener('change', ()=>setInterval(()=>(selectTag.value === "all")? resetPrint(data) :(selectTag.value === "animals")?  resetPrint(animals) :(selectTag.value === "vegetable")?  resetPrint(vegetable)   : resetPrint(user),100));
 //reset at start + under the funciton's
 resetPrint(data);
-function resetPrint(array){ bana.innerHTML = ""; prntHtml(array)};
+function resetPrint(array){ row.innerHTML = ""; prntHtml(array)};
 function prntHtml(array){array.forEach(obj => { const {color, name, prefix} = obj;
-        bana.innerHTML += `
+        row.innerHTML += `
         <div class="col-12 col-md-4 col-lg-2 mt-4 mb-4">
             <div class="card text-center pb-5 pt-5" style="color: #${randomColor(aF, 10)}">
                 <h1><i class="${prefix}solid ${prefix}${name} "></i></h1>
